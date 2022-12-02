2ed4c8fe929b32fb2b725538eb9d077a44efe683
YONDU\rasistores <rasistores>
2022-12-02 13:40:40 +0800

    rohan-project-file

---
+++ rohan/src/main/java/com/yondu/unversity/rohan/RohanApplication.java
@@ -0,0 +1,13 @@
+package com.yondu.unversity.rohan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RohanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RohanApplication.class, args);
+	}
+
+}
